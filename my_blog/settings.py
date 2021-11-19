3952c12c9c4393a4beabcd5f0f3d77d6e6364620
Kuro <ivankuro752@gmail.com>
2021-11-20 06:06:27 +0700

    first

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y1h6gah_=4!-)fl2qc)uvj50dg9hy=7g-)l#v*m5vq&=+m((0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'spilled-bits.herokuapp.com']
 
