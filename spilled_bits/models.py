bf03543c9e1711bfd70eeaed007d0a2df984cd8c
Kuro <ivankuro752@gmail.com>
2021-11-20 07:48:34 +0700

    first

--- spilled_bits/models.py
+++ spilled_bits/models.py
@@ -18,7 +18,7 @@ class Article(models.Model):
 
     title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
-    slug = models.SlugField(max_length=50, blank=True, editable=False)
+    slug = models.SlugField(max_length=200, blank=True, editable=False)
     date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=15, choices=STATUS, blank=True, null=True)
     category = models.ManyToManyField(Category)
