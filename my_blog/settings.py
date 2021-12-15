4f678ad7f3215aa0b2fffa625f750fc485dd7da4
Kuro <ivankuro752@gmail.com>
2021-12-15 16:03:14 +0700

    about

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "Optional value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
