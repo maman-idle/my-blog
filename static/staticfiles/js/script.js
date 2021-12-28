d89a14e68a3885f0690436473e8bcde330f79579
Kuro <ivankuro752@gmail.com>
2021-12-28 11:06:23 +0700

    foot

--- static/staticfiles/js/script.js
+++ static/staticfiles/js/script.js
@@ -1,1 +1,7 @@
-C:/Users/USER/Documents/Software Dev/Python Projects/Django Projects/my_blog/spilled_bits/static/js/script.js
+textarea = document.querySelector("#autoresizing");
+textarea.addEventListener('input', autoResize, false);
+      
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
--- static/staticfiles/tinymce/langs/readme.md
+++ static/staticfiles/tinymce/langs/readme.md
@@ -1,1 +1,3 @@
-C:/Users/USER/.virtualenvs/Django_Projects-k_kfBoyd/lib/site-packages/tinymce/static/tinymce/langs/readme.md
+This is where language files should be placed.
+
+Please DO NOT translate these directly use this service: https://www.transifex.com/projects/p/tinymce/
