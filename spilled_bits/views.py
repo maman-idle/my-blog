4c3936c9b21a0da7431ae60ffce4da0a1dfc8f34
maman-idle <faturahman.ivan5@gmail.com>
2022-02-13 08:57:50 +0700

    nav_bar

--- spilled_bits/views.py
+++ spilled_bits/views.py
@@ -1,5 +1,3 @@
-from asyncio.windows_events import NULL
-from django.template.defaultfilters import date, title
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Article
